6253c6623a69fa65c14b873f6469f05d83b8a8b0
John Smoker <testgits@yandex.ru>
2021-04-12 01:05:33 +0500

    Added matcher for api

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,25 +1,26 @@
 package tests;
 
-import api.Auth;
 import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tests.steps.ApiSteps;
 import tests.steps.WebSteps;
 
-import java.util.Map;
-
 @Owner("f27")
+@DisplayName("Login tests")
 public class LoginTests extends TestBase{
 
     @Test
     @Tag("API")
+    @DisplayName("Verify login via API")
     void loginOnlyApiTest() {
         ApiSteps.verifyLogin(TestData.getUserEmail());
     }
 
     @Test
     @Tag("MIXED")
+    @DisplayName("Verify login via UI")
     void mixedLoginTest() {
         WebSteps.setCookies(authCookies);
         WebSteps.verifyLogin(TestData.getUserEmail());
@@ -27,6 +28,7 @@ public class LoginTests extends TestBase{
 
     @Test
     @Tag("UI")
+    @DisplayName("Login via UI, verify login")
     void uiLoginTest() {
         WebSteps.login(TestData.getUserEmail(), TestData.getUserPassword());
         WebSteps.verifyLogin(TestData.getUserEmail());
