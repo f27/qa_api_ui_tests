37e7010fabc32f5e62c6c24fcf6eff37585b2bfe
John Smoker <testgits@yandex.ru>
2021-04-14 12:26:34 +0500

    Email pattern

--- src/test/java/api/matchers/ApiMatchers.java
+++ src/test/java/api/matchers/ApiMatchers.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ApiMatchers {
-    private static final String myEmailOnMainPattern = "<a href=\"/customer/info\" class=\"account\">(.*?[@].*?)</a>";
+    private static final String myEmailOnMainPattern = "<a href=\"/customer/info\" class=\"account\">(\\w+@\\w+.\\w+)</a>";
 
     public static String getEmailFromMain(String pageText) {
         Pattern pattern = Pattern.compile(myEmailOnMainPattern);
